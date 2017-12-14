0b0fa68fc9164ad9be63ffeb1585d45f86f43170
oconnes9 <oconnes9@tcd.ie>
2017-12-14 13:36:31 +0000

    One worker

---
+++ Master.py
@@ -0,0 +1,91 @@
+from socket import *
+import requests
+from collections import deque
+import shutil
+
+trees = []
+blobUrlList = []
+githubUrl = 'https://api.github.com/repos/oconnes9/Distributed_System/commits'
+
+def getTrees(githubUrl):
+    print("1")
+    
+    payloadHeaders = getParamHeaders()
+
+    resp = requests.get(githubUrl,   params=payloadHeaders[0], headers=payloadHeaders[1])    # get the commit page of the github url
+    
+    for item in resp.json():
+        trees.append(item['commit']['tree']['url']) # parse out the tree URL's from the commit page
+    
+    return trees
+
+
+def blobList(githubUrl, trees):
+    print("2")
+    payloadHeaders = getParamHeaders()
+    for blobUrl in trees:
+        resp = requests.get(blobUrl,   params=payloadHeaders[0], headers=payloadHeaders[1])
+        
+        tree = resp.json()['tree']
+        
+        for item in tree:
+            fileUrl = item['url']
+            filename = item['path']
+            
+            urlFilename = fileUrl + '|' + filename
+            blobUrlList.append(urlFilename)
+
+    return blobUrlList
+
+def getParamHeaders():
+    print("3")
+    with open('github-token.txt', 'r') as tmpFile:
+        token = tmpFile.read()     # get the token from a text file in current directory
+    payload = {'access_token': token}
+    headers = {'Accept': 'application/vnd.github.v3.raw'}
+    
+    return (payload, headers)
+
+def send_work(blobUrlList):
+    print("4")
+    serverName = 'localhost'
+    serverPort = 1111
+    totalCC = 0
+    count = 0
+    
+    # send URL's one by one to the worker
+    for blobUrl in blobUrlList:
+        manager_socket = socket(AF_INET, SOCK_STREAM)
+        manager_socket.connect((serverName,serverPort))
+        manager_socket.send(blobUrl.encode())
+        print(blobUrl + " sent to worker.")
+        
+        reply = manager_socket.recv(1024)   # receive the cyclomatic complexity for the file at the current raw URL
+        reply = reply.decode()
+        print(reply + " received from worker.")
+        
+        if reply != "-1" and reply is not None:
+            reply = int(reply)
+            totalCC += reply       # increment total commit CC with the reply from the worker
+            count += 1
+        manager_socket.close()
+    print ("Count = " + str(count))
+    return totalCC
+
+def main():
+
+    print("Getting Tree URL list...")
+    trees = getTrees(githubUrl)      # get the list of tree URL's from the project's commits
+    print("Tree URL list received...")
+    print("Gettng blob URL's...")
+    blobUrlList = blobList(githubUrl, trees)    # get blob URLs of each tree's
+    print("Commenced sending of raw URL's to worker...")
+    total_cc = send_work(blobUrlList)     # send the raw URL's to the worker
+    
+    print("Total CC is: " + str(totalCC))
+    
+    avgCC = totalCC/len(blobUrlList)
+    print("Average CC is: " + str(avgCC))
+
+if __name__ == "__main__":
+    main()
---
+++ Node.py
@@ -0,0 +1,105 @@
+from socket import *
+import os
+from radon.complexity import SCORE
+from radon.cli.harvest import CCHarvester
+from radon.cli import Config
+import requests
+from time import gmtime, strftime
+import os.path
+import sys
+import shutil
+
+from re import match
+
+ccConfig = Config(
+                   exclude='',
+                   ignore='venv',
+                   order=SCORE,
+                   no_assert=True,
+                   show_closures=False,
+                   min='A',
+                   max='F',
+                   )
+
+def getHeader():
+    with open('github-token.txt', 'r') as tmp_file:
+        token = tmp_file.read()     # get the token from a text file in current directory
+
+    payload = {'access_token': token}
+    headers = {'Accept': 'application/vnd.github.v3.raw'}
+    
+#print(token)
+    return (payload, headers)
+
+
+def check_py(filename):
+    return True if match('.*\.py', filename) is not None else False
+
+def calc_CC(raw_url, ccConfig):
+    
+    blob_url = raw_url.split('|')[0]
+    filename = raw_url.split('|')[1]
+    
+    payload_headers = getHeader()
+    
+    flag = check_py(filename)
+    if flag == True:
+        
+        resp = requests.get(blob_url,   params=payload_headers[0], headers=payload_headers[1])
+        
+        file_path = filename
+        
+        with open(file_path, 'w') as tmp_file:
+            tmp_file.write(resp.text)
+        tmp_file.close()
+        
+        
+        CC_file_get = open(file_path, 'r')
+        results = CCHarvester(file_path, ccConfig).gobble(CC_file_get)
+        CC_file_get.close()
+        os.remove(file_path)
+        
+        file_cc = 0
+        
+        for i in results:
+            print (i.complexity)
+            file_cc += int(i.complexity)
+        
+        #avg_cc = file_cc/ len(results)
+        print("Complexity of file: " + str(file_cc))
+        #print("Average complexity of file: " + str(avg_cc))
+        return file_cc
+    else:
+        return -1
+
+def receive_work():
+    
+    CC_socket = socket(AF_INET, SOCK_STREAM)
+    serverName = 'localhost'
+    serverPort = 1111
+    CC_socket.bind(('', serverPort))
+    CC_socket.listen(1)
+    
+    while True:
+        connectionSocket, addr = CC_socket.accept()
+        raw_url = connectionSocket.recv(1024)
+        raw_url = raw_url.decode()
+        
+        print("RECEIVED: " + raw_url)
+        
+        file_cc = calc_CC(raw_url, ccConfig)
+        
+        file_cc = str(file_cc)
+        print(file_cc)
+        connectionSocket.send(file_cc.encode())
+    
+    connectionSocket.close()
+
+
+def main():
+    
+    print("Worker is ready to receive...")
+    receive_work()
+
+if __name__ == "__main__":
+    main()
